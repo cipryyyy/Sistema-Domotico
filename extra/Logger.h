32d59fdb4fd5492a89bfb93c5c0257cc34602cfd
Giacomo Giorgi <giacomo1215@gmail.com>
2025-01-12 17:06:16 +0100

    Piccole sistemazioni nel logger

--- extra/Logger.h
+++ extra/Logger.h
@@ -13,15 +13,11 @@ class Logger {
     bool debugMode;           // Modalità debug
 
     // Funzione per ottenere il timestamp corrente
-    std::string getTimestamp(bool isFileTitle = false) {
+    std::string getTimestamp() {
       std::time_t now = std::time(nullptr);
       std::tm* localTime = std::localtime(&now);
       char buffer[20];
-      if (isFileTitle) {
-        std::strftime(buffer, sizeof(buffer), "%Y%m%d", localTime);
-      } else {
-        std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", localTime);
-      }
+      std::strftime(buffer, sizeof(buffer), "%Y%m%d", localTime);
       return std::string(buffer);
     }
 
@@ -30,7 +26,7 @@ class Logger {
         
     Logger(bool debug = false)                                                            // Costruttore
       : debugMode(debug) {    
-          logFileName = "../logs/log_" + getTimestamp(true) + ".txt";                     // Crea il nome del file di log
+          logFileName = "../logs/log_" + getTimestamp() + ".txt";                         // Crea il nome del file di log
           logFile.open(logFileName, std::ios::app);                                       // Apre il file di log in modalità append
           if (!logFile.is_open()) {                                                       // Se il file di log non è aperto
             std::cerr << "Impossibile aprire file: " << logFileName << std::endl;         // Stampa un messaggio di errore
@@ -52,28 +48,29 @@ class Logger {
         }       
     }       
 
-    void log(LogLevel level,const std::string& time = "", const std::string& message) {   // Funzione per scrivere un messaggio di log
-      if (!logFile.is_open() || (level == DEBUG && !debugMode)) return;                   // Se il file di log non è aperto o il livello di log è DEBUG e la modalità debug è disattivata, esce
-
-      static const char* levelStr[] = { "DEBUG", "USERINPUT", "EVENT", "ERROR", "INFO" }; // Array di stringhe per i livelli di log
-      std::ostringstream logEntry;                                                        // Stream per il messaggio di log
-
-      if (time.empty()) {                                                                 // Se il timestamp non è specificato
-        logEntry << "[" << levelStr[level] << "] "                                        // Aggiunge il livello di log al messaggio di log
-                 << message << std::endl;                                                 // Aggiunge il messaggio al messaggio di log
-      } else {
-        logEntry << "[" << time << "] "                                                   // Aggiunge il timestamp al messaggio di log
-                << "[" << levelStr[level] << "] "                                         // Aggiunge il livello di log al messaggio di log
-                << message << std::endl;                                                  // Aggiunge il messaggio al messaggio di log
-      }
-
-      logFile << logEntry.str();                                                          // Scrive il messaggio sul file di log
-      logFile.flush();                                                                    // Svuota il buffer del file di log
-    }     
-
-    void close() {                                                                        // Funzione per chiudere il file di log
-      if (logFile.is_open()) {                                                            // Se il file di log è aperto
-        logFile.close();                                                                  // Chiude il file di log
+    void log(LogLevel level, const std::string& time = "", const std::string& message = "") {  // Funzione per scrivere un messaggio di log
+      if (!logFile.is_open() || (level == DEBUG && !debugMode)) return;                        // Se il file di log non è aperto o il livello di log è DEBUG e la modalità debug è disattivata, esc
+    
+      static const char* levelStr[] = { "DEBUG", "USERINPUT", "EVENT", "ERROR", "INFO" };      // Array di stringhe per i livelli di log
+      std::ostringstream logEntry;                                                             // Stream per il messaggio di log
+    
+      if (time.empty()) {                                                                      // Se il timestamp non è specificato
+        logEntry << "[" << levelStr[level] << "]"                                              // Aggiunge il livello di log al messaggio di log
+                 << message << std::endl;                                                      // Aggiunge il messaggio al messaggio di log
+      } else {     
+        logEntry << "[" << time << "] "                                                        // Aggiunge il timestamp al messaggio di log
+                << "[" << levelStr[level] << "]: "                                             // Aggiunge il livello di log al messaggio di log
+                << message << std::endl;                                                       // Aggiunge il messaggio al messaggio di log
+      }    
+    
+      logFile << logEntry.str();                                                               // Scrive il messaggio sul file di log
+      logFile.flush();                                                                         // Svuota il buffer del file di log
+    }          
+    
+    void close() {                                                                             // Funzione per chiudere il file di log
+      log(LogLevel::EVENT, std::string("Applicazione terminata con successo"));                // Scrive un messaggio di log
+      if (logFile.is_open()) {                                                                 // Se il file di log è aperto
+        logFile.close();                                                                       // Chiude il file di log
       }
     }
 };
